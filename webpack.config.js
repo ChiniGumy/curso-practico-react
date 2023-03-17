4275c73beea7ad3d956d44f6fc060ad7156be078
David Sánchez Romero <dsebastiansr@hotmail.com>
2023-03-17 14:57:18 -0500

    [CLASS: Imagenes y alias en webpack]

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
 
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias: {
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@containers': path.resolve(__dirname, 'src/containers'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@styles': path.resolve(__dirname, 'src/styles'),
+			'@icons': path.resolve(__dirname, 'src/assets/icons'),
+			'@logos': path.resolve(__dirname, 'src/assets/logos'),
+		},
 	},
 
 	module: {
@@ -40,6 +48,11 @@ module.exports = {
 					"css-loader",
 					"sass-loader",
 				]
+			},
+
+			{
+				test: /\.*(png|svg|jpg|gif|jpeg)$/,
+				type: 'asset'
 			}
 		]
 	},
