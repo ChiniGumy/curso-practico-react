80ad87b9c7a62d38edf0a47e34efccd199a5c6f7
David Sánchez Romero <dsebastiansr@hotmail.com>
2023-03-21 13:33:31 -0500

    [CLASS] useRef y Formularios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import '@styles/Login.scss';
 
 import logoYardSale from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
+
+    const form = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const formData = new FormData(form.current);
+
+        const data = {
+            username: formData.get('email'),
+            password: formData.get('password')
+        };
+        console.log(data);
+    };
+
 	return (
         <div className="login__container">
-
             <div className="login__form-container">
                 <img src={logoYardSale} alt="logo" className="login__logo" />
 
-                <form action="/" className="login__form">
+                <form action="/" className="login__form" ref={form}>
                     <label htmlFor="email" className="login__label">
                         Email address
                     </label>
                     <input
                         type="text"
-                        id="email"
+                        name="email"
                         placeholder="platzi@example.cm"
                         className="login__input login__input-email"
                     />
@@ -26,19 +39,27 @@ const Login = () => {
                     </label>
                     <input
                         type="password"
-                        id="password"
+                        name="password"
                         placeholder="*********"
                         className="login__input login__input-password"
                     />
 
-                    <input
+                    <button
+                        onClick={handleSubmit}
                         type="submit"
                         value="Log in"
                         className="login__primary-button login__login-button"
-                    />
+                    >
+                        Log in
+                    </button>
+
                     <a href="/password-recovery">Forgot my password</a>
                 </form>
-                <button className="login__secondary-button login__signup-button">Sign up</button>
+                <button
+                    className="login__secondary-button login__signup-button"
+                >
+                    Sign up
+                </button>
             </div>
         </div>
     );
