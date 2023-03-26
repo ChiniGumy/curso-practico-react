46b36aa58dbcf55dd69879adde2dc2035cd3197e
David Sánchez Romero <dsebastiansr@hotmail.com>
2023-03-26 13:25:58 -0500

    [CLASS] Eliminando productos del carrito

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import AppContext from '@context/AppContext'
 import '@styles/OrderItem.scss';
 
 import closeIcon from '@icons/icon_close.png';
 
 const OrderItem = ({product}) => {
+
+	const {removeFromCart} = useContext(AppContext);
+
+	const handleRemove = product => {
+		removeFromCart(product);
+	}
+
 	return (
 		<div className="OrderItem">
 			<figure>
@@ -11,7 +19,7 @@ const OrderItem = ({product}) => {
 			</figure>
 			<p>{product.title}</p>
 			<p>${product.price}</p>
-			<img src={closeIcon} alt="close" />
+			<img src={closeIcon} alt="close" onClick={() => handleRemove(product)}/>
 		</div>
 	);
 }
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -14,9 +14,17 @@ const useInitialState = () => {
 		});
 	};
 
+	const removeFromCart = (payload) => {
+		setState({
+			...state,
+			cart: [...state.cart.filter(item => item.id !== payload.id)]
+		});
+	};
+
 	return {
 		state,
-		addToCart
+		addToCart,
+		removeFromCart
 	};
 };
 
